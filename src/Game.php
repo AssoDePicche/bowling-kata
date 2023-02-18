20bbabb735bf2a5b73c93fac85662b3a7522043a
AssoDePicche <samuelprado730@gmail.com>
2023-02-18 15:27:08 -0300

    feat: add strike score

--- src/Game.php
+++ src/Game.php
@@ -25,13 +25,19 @@ final class Game
     $score = $frameIndex = 0;
 
     for ($frame = 0; $frame < 10; $frame++) {
-      if ($this->isSpare($frameIndex)) {
+      if ($this->rolls[$frameIndex] === 10) {
+        $score += 10 + $this->rolls[$frameIndex + 1] + $this->rolls[$frameIndex + 2];
+
+        $frameIndex++;
+      } else if ($this->isSpare($frameIndex)) {
         $score += 10 + $this->rolls[$frameIndex + 2];
+
+        $frameIndex += 2;
       } else {
         $score += $this->rolls[$frameIndex] + $this->rolls[$frameIndex + 1];
-      }
 
-      $frameIndex += 2;
+        $frameIndex += 2;
+      }
     }
 
     return $score;
