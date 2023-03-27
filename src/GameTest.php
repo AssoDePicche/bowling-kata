b046b5d969cfcdad02c26bc27c53ff239ddab7a2
AssoDePicche <samuelprado730@gmail.com>
2023-03-27 08:24:21 -0300

    refactor: extract game instantiation to setup function

--- src/GameTest.php
+++ src/GameTest.php
@@ -4,81 +4,74 @@ declare(strict_types=1);
 
 namespace App;
 
-use PHPUnit\Framework\TestCase;
-
-final class GameTest extends TestCase
+final class GameTest extends \PHPUnit\Framework\TestCase
 {
+  private Game $game;
+
+  public function setUp(): void
+  {
+    $this->game = new Game;
+  }
+
   public function test_should_score_zero_for_gutter_game(): void
   {
-    $game = $this->rollMany(20, 0);
+    $this->rollMany(20, 0);
 
-    $this->assertEquals(0, $game->score());
+    $this->assertEquals(0, $this->game->score());
   }
 
   public function test_should_score_twenty_for_all_ones_game(): void
   {
-    $game = $this->rollMany(20, 1);
+    $this->rollMany(20, 1);
 
-    $this->assertEquals(20, $game->score());
+    $this->assertEquals(20, $this->game->score());
   }
 
   public function test_should_score_sixteen_with_a_spare_followed_by_a_three_ball(): void
   {
-    $game = $this->rollSpare();
+    $this->rollSpare();
 
-    $game->roll(3);
+    $this->game->roll(3);
 
-    $this->assertEquals(16, $game->score());
+    $this->assertEquals(16, $this->game->score());
   }
 
   public function test_should_score_twenty_four_for_a_strike_followed_by_a_three_and_four_balls(): void
   {
-    $game = $this->rollStrike();
+    $this->rollStrike();
 
-    $game->roll(3);
+    $this->game->roll(3);
 
-    $game->roll(4);
+    $this->game->roll(4);
 
-    $rest = $this->rollMany(16, 0);
+    $this->rollMany(16, 0);
 
-    $this->assertEquals(24, $game->score() + $rest->score());
+    $this->assertEquals(24, $this->game->score());
   }
 
   public function test_should_score_three_hundred_for_a_perfect_game(): void
   {
-    $game = $this->rollMany(12, 10);
+    $this->rollMany(12, 10);
 
-    $this->assertEquals(300, $game->score());
+    $this->assertEquals(300, $this->game->score());
   }
 
-  private function rollMany(int $rolls, int $pins): Game
+  private function rollMany(int $rolls, int $pins): void
   {
-    $game = new Game;
-
     for ($i = 0; $i < $rolls; $i++) {
-      $game->roll($pins);
+      $this->game->roll($pins);
     }
-
-    return $game;
   }
 
-  private function rollSpare(): Game
+  private function rollSpare(): void
   {
-    $game = new Game;
+    $this->game->roll(5);
 
-    $game->roll(5);
-
-    $game->roll(5);
-
-    return $game;
+    $this->game->roll(5);
   }
 
-  private function rollStrike(): Game
+  private function rollStrike(): void
   {
-    $game = new Game;
-
-    $game->roll(10);
-
-    return $game;
+    $this->game->roll(10);
   }
 }
